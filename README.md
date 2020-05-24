052b19d20efc309df9f1f9f1015ff106d995a3fb
Gijs de Jong <gijs_d.j@hotmail.com>
2020-05-24 18:27:32 +0200

    Stick Hero can be played!

--- README.md
+++ README.md
@@ -15,4 +15,4 @@ go test ./...
 ## Resources
 - https://github.com/spf13/cobra
 - https://towardsdatascience.com/how-to-create-a-cli-in-golang-with-cobra-d729641c7177
-- https://github.com/engineer-man/youtube/tree/master/105
+- https://github.com/engineer-man/youtube/tree/master/105 + https://www.youtube.com/watch?v=Du__JfXqsAs
--- cmd/play_stick_hero.go
+++ cmd/play_stick_hero.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gdejong/adb-golang/pkg/adb"
 	"github.com/spf13/cobra"
 	"log"
+	"time"
 )
 
 func init() {
@@ -14,6 +15,9 @@ var playStickHeroCommand = &cobra.Command{
 	Use:   "adb:play-stick-hero",
 	Short: "Play the Stick Hero game",
 	Run: func(cmd *cobra.Command, args []string) {
+		// This is basically just a Golang implementation of https://www.youtube.com/watch?v=Du__JfXqsAs
+		// Check out his videos, he has a great YouTube channel!
+
 		// Verify the connection to the device.
 		_, err := adb.GetAdbSerialNumber()
 		adb.HandleErr(err)
@@ -23,9 +27,56 @@ var playStickHeroCommand = &cobra.Command{
 			log.Fatalln("Device screen is off, please unlock the device.")
 		}
 
-		img := adb.MakeScreenshot()
+		for {
+			img := adb.MakeScreenshot()
 
-		adb.StoreImage(img, "test.png")
-		adb.Swipe(100)
+			ignore := true
+			isBlack := true
+			transitions := make([]int, 3)
+			transitionCount := 0
+
+			for x := 0; x < img.Bounds().Max.X; x++ {
+				color := img.At(x, 1800)
+				r, g, b, _ := color.RGBA()
+
+				// Ignore pixels until we encounter the first black area.
+				if ignore && (r+g+b != 0) {
+					continue
+				}
+
+				ignore = false
+
+				if isBlack && (r+g+b != 0) {
+					isBlack = !isBlack
+
+					transitions[transitionCount] = x
+					transitionCount++
+
+					continue
+				}
+
+				if !isBlack && (r+g+b == 0) {
+					isBlack = !isBlack
+
+					transitions[transitionCount] = x
+					transitionCount++
+
+					continue
+				}
+			}
+
+			start := transitions[0]
+			target1 := transitions[1]
+			target2 := transitions[2]
+
+			gap := target1 - start
+			target := target2 - target1
+
+			distance := int(float64(gap+target/2) * .98)
+
+			adb.Swipe(distance)
+
+			time.Sleep(time.Millisecond * 2500)
+		}
 	},
 }
